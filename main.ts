1cc6a5c3f1c1ab669e983cb910d3605d051f309f
SvenArcticFox <74692162+SvenArcticFox@users.noreply.github.com>
2024-11-17 17:29:11 -0500

    Add main.ts with beginning functions and test cases

---
+++ main.ts
@@ -0,0 +1,23 @@
+import foldl from "./foldl";
+import foldr from "./foldr";
+
+// test lists (arrays)
+const testArr1: number[] = [1,2,3,4,5,6,7,8,9];
+const testArr2: string = "abcdefgh";
+
+console.log(`Test number arry: ${testArr1}`);
+console.log(`Test string (character array): ${testArr2}`);
+console.log(''); // add new line
+
+// functions
+function add(i: number, j: number): number {
+    return i + j;
+}
+
+function multiply(i: number, j: number): number {
+    return i + j;
+}
+
+const addedNumbersFoldr = foldr(add, testArr1, 0);
+console.log(`Addition of added numbers: ${addedNumbersFoldr}`);
+
