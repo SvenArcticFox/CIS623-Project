9dfa5924cc8bfca770da28bed258d99e7fc89bd8
SvenArcticFox <74692162+SvenArcticFox@users.noreply.github.com>
2024-11-18 22:40:47 -0500

    Add reverse functions and split string to array

--- main.ts
+++ main.ts
@@ -3,7 +3,7 @@ import foldr from "./foldr";
 
 // test lists (arrays)
 const testArr1: number[] = [1,2,3,4,5,6,7,8,9];
-const testArr2: string = "abcdefgh";
+const testArr2: string[] = "abcdefghijklmnopqrstuvwxyz".split('');
 
 console.log(`Test number arry: ${testArr1}`);
 console.log(`Test string (character array): ${testArr2}`);
@@ -15,11 +15,47 @@ function add(i: number, j: number): number {
 }
 
 function multiply(i: number, j: number): number {
-    return i + j;
+    return i * j;
 }
 
-const addedNumbersFoldr = foldr(add, testArr1, 0);
-console.log(`Addition of added numbers using foldr: ${addedNumbersFoldr}`);
+const addedNumbersFoldr = foldr<number, number>(add, testArr1, 0);
+console.log(`Addition of numbers using foldr: ${addedNumbersFoldr}`);
 
-const addedNumberFoldl = foldl(add, testArr1, 0);
-console.log(`Addition of added numbers using foldl: ${addedNumberFoldl}`);
+const addedNumberFoldl = foldl<number, number>(add, testArr1, 0);
+console.log(`Addition of numbers using foldl: ${addedNumberFoldl}`);
+
+const multipliedNumbersFoldr = foldr<number, number>(multiply, testArr1, 1);
+console.log(`Multiplication of numbers using foldr: ${multipliedNumbersFoldr}`);
+
+const multipliedNumbersFoldl = foldl<number, number>(multiply, testArr1, 1);
+console.log(`Multiplicaion of numbers using foldl: ${multipliedNumbersFoldl}`);
+
+const reverseFoldr = <t>(arr: Array<t>): Array<t> => {
+    return foldr<t, Array<t>>((value: t, accumulator: Array<t>): Array<t> => {
+        const acc = accumulator;
+        acc.push(value);
+        return acc;
+    }, arr, new Array<t>);
+};
+
+const reverseArrFoldr = reverseFoldr<number>(testArr1);
+console.log(`Reverse numbers using foldr: ${reverseArrFoldr}`);
+
+const reverseFoldl = <t>(arr: Array<t>): Array<t> => {
+    return foldl<t, Array<t>>((value: t, accumulator: Array<t>): Array<t> => {
+        const acc = accumulator;
+        acc.unshift(value);
+        return acc;
+    }, arr, new Array<t>);
+};
+
+const reversArrFoldl = reverseFoldl<number>(testArr1);
+console.log(`Reverse numbers using foldl: ${reversArrFoldl}`);
+
+const reversArr2Foldr = reverseFoldr<string>(testArr2);
+console.log(`Reverse char array (string) using foldr: ${reversArr2Foldr}`);
+
+const reversArr2Foldl = reverseFoldl<string>(testArr2);
+console.log(`Reverse char array (string) using foldr: ${reversArr2Foldl}`);
+
+console.log('');
